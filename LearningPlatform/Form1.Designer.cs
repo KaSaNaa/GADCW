857ec26097d029ecef51e90fdaea0ca4085e744a
Pasindu Ranawaka <pasindudeshitha123@gmail.com>
2023-02-19 03:43:56 +0530

    created a FileUploadForm branch in order to sepearate and create a file upload window for the main.

--- LearningPlatform/Form1.Designer.cs
+++ LearningPlatform/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Btn_UploadFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_UploadFiles
+            // 
+            this.Btn_UploadFiles.Location = new System.Drawing.Point(307, 186);
+            this.Btn_UploadFiles.Name = "Btn_UploadFiles";
+            this.Btn_UploadFiles.Size = new System.Drawing.Size(126, 23);
+            this.Btn_UploadFiles.TabIndex = 0;
+            this.Btn_UploadFiles.Text = "Upload files";
+            this.Btn_UploadFiles.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_UploadFiles);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn_UploadFiles;
     }
 }
 
