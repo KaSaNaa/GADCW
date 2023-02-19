982462971644141c21644310c65b1eb814b7ef25
Pasindu Ranawaka <pasindudeshitha123@gmail.com>
2023-02-19 13:56:41 +0530

    useless

--- LearningPlatform/Form1.cs
+++ LearningPlatform/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Azure.Storage.Blobs;
 
 namespace LearningPlatform
 {
@@ -15,6 +16,19 @@ namespace LearningPlatform
         public Form1()
         {
             InitializeComponent();
+        }
+        public static void GetBlobServiceClientSAS(ref BlobServiceClient blobServiceClient,
+            string accountName, string sasToken)
+        {
+            string blobUri = "https://kasana.blob.core.windows.net/learningplatform?sp=racwdl&st=2023-02-18T23:01:34Z&se=2023-04-29T07:01:34Z&spr=https&sv=2021-06-08&sr=c&sig=TjF0tIhjvADn9abJ1VHKEFEZGjUjcID3F4Ej9keiops%3D";
+
+            blobServiceClient = new BlobServiceClient
+                (new Uri($"{blobUri}?{"sp = racwdl & st = 2023 - 02 - 18T23:01:34Z&se=2023-04-29T07:01:34Z&spr=https&sv=2021-06-08&sr=c&sig=TjF0tIhjvADn9abJ1VHKEFEZGjUjcID3F4Ej9keiops%3D}"), null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = ""
         }
     }
 }
