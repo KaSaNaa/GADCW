4919c201a09da2f024e4876d53cabf4a1351ecac
Pasindu Ranawaka <pasindudeshitha123@gmail.com>
2023-02-27 14:18:30 +0530

    Revert "removed irrelevant files and folders to this branch"
    This reverts commit 33e121dadaea02f337c161926f06c413f491e2f3.

---
+++ Learning_Platform_Login/Learning_Platform_Login/Learning_Platform/Lecturer_Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Learning_Platform
+{
+    public partial class Lecturer_Main : Form
+    {
+        public Lecturer_Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Learning_Platform_Login/Learning_Platform_Login/Learning_Platform/Sign_In_Student_Pwd.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Learning_Platform
+{
+    public partial class Sign_In_Student_Pwd : Form
+    {
+        public Sign_In_Student_Pwd()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con;
+        SqlCommand cmd;
+        private void btn_confirm_Click(object sender, EventArgs e)
+        {
+            string username = txt_username.Text;
+            string password = txt_pwd.Text;
+            string confirm_password = txt_con_pwd.Text;
+
+            try
+            {
+                if (string.IsNullOrEmpty(username))
+                {
+                    MessageBox.Show("Please enter your user name with characters.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                else if (string.IsNullOrEmpty(password))
+                {
+                    MessageBox.Show("Please enter a password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                else if (password != confirm_password)
+                {
+                    MessageBox.Show("The password and confirm password fields do not match. Please re-enter your password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            } 
+            
+            catch (Exception)
+            {
+                MessageBox.Show("Please try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            con = new SqlConnection("Data Source=DESKTOP-CTNVSJ1;Initial Catalog=Sign_Up_Details;Integrated Security=True");
+            con.Open();
+            cmd = new SqlCommand("UPDATE Student SET Student_Password = '"+txt_pwd.Text+"', Student_Confirm = '"+txt_con_pwd.Text+"' WHERE Student_Username = '"+txt_username.Text+"'", con);
+
+            int i = cmd.ExecuteNonQuery();
+
+            if (i == 1)
+            {
+                MessageBox.Show("New Password is created", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else
+            {
+                MessageBox.Show("Please check again the fields and enter the valid information", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            con.Close();
+        }
+    }
+}
