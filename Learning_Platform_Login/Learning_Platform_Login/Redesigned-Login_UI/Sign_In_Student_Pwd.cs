5dd452dd04793f3da901022220eb22c9677e677a
Pasindu Ranawaka <pasindudeshitha123@gmail.com>
2023-02-27 10:57:47 +0530

    added redesign login interface

---
+++ Learning_Platform_Login/Learning_Platform_Login/Redesigned-Login_UI/Sign_In_Student_Pwd.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Redesigned_Login_UI
+{
+    public partial class Sign_In_Student_Pwd : MetroSet_UI.Forms.MetroSetForm
+    {
+        public Sign_In_Student_Pwd()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con;
+        SqlCommand cmd;
+
+        private void Btn_SignIn_Click(object sender, EventArgs e)
+        {
+            string username = TxtB_Username.Text;
+            string password = TxtB_Password.Text;
+            string confirm_password = TxtB_ConfirmPswd.Text;
+
+            try
+            {
+                if (string.IsNullOrEmpty(username))
+                {
+                    MessageBox.Show("Please enter your user name with characters.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                else if (string.IsNullOrEmpty(password))
+                {
+                    MessageBox.Show("Please enter a password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                else if (password != confirm_password)
+                {
+                    MessageBox.Show("The password and confirm password fields do not match. Please re-enter your password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Please try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            con = new SqlConnection("Data Source=DESKTOP-CTNVSJ1;Initial Catalog=Sign_Up_Details;Integrated Security=True");
+            con.Open();
+            cmd = new SqlCommand("UPDATE Student SET Student_Password = '" + TxtB_Password.Text + "', Student_Confirm = '" + TxtB_ConfirmPswd.Text + "' WHERE Student_Username = '" + TxtB_Username.Text + "'", con);
+
+            int i = cmd.ExecuteNonQuery();
+
+            if (i == 1)
+            {
+                MessageBox.Show("New Password is created", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else
+            {
+                MessageBox.Show("Please check again the fields and enter the valid information", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            con.Close();
+        }
+    }
+}
