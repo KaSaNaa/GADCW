1de50db2f9aef5df62515fcc2fd3da151b429f9b
psychicPerfectionist <thilothmarathnasinghe@gmail.com>
2023-02-28 13:39:07 +0530

    edited AddNewQuestion form

--- Online Evaluation System/Online Evaluation System/Lecturer_usercontrols/AddNewQuestion.cs
+++ Online Evaluation System/Online Evaluation System/Lecturer_usercontrols/AddNewQuestion.cs
@@ -58,8 +58,7 @@ namespace Online_Evaluation_System.Lecturer_usercontrols
 
         private void btn_next_Click(object sender, EventArgs e)
         {
-            
-            
+
                 String activity = txt_activity.Text;
                 String qno = questionNo.ToString();
                 String question = txt_question.Text;
@@ -69,9 +68,36 @@ namespace Online_Evaluation_System.Lecturer_usercontrols
                 String option4 = txt_option4.Text;
                 String ans = txt_answer.Text;
 
-               
+            query = "select * from Questions where activity = '" + activity + "' and question = '" + question + "' and option1 = '" + option1 + "' and option2 = '" + option2 + "' and option3 = '" + option3 + "' and option4 = '" + option4 + "' and ans = '" + ans + "'";
+            ds = fn.getData(query);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                MessageBox.Show("This question already exists in the " + activity + " activity set.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
-                query = "insert into Questions(activity, qno, question, option1, option2, option3, option4, ans) values('" + activity + "','" + qno + "','" + question + "','" + option1 + "','" + option2 + "','" + option3 + "','" + option4 + "','" + ans + "')";
+            if (string.IsNullOrWhiteSpace(question) || string.IsNullOrWhiteSpace(option1) || string.IsNullOrWhiteSpace(option2) || string.IsNullOrWhiteSpace(option3) || string.IsNullOrWhiteSpace(option4) || string.IsNullOrWhiteSpace(ans))
+            {
+                MessageBox.Show("Please fill in all the fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (ans != option1 && ans != option2 && ans != option3 && ans != option4)
+            {
+                MessageBox.Show("Answer does not match any of the options.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int activityValue;
+            if (!int.TryParse(activity, out activityValue) || activityValue <= 0)
+            {
+                MessageBox.Show("Invalid activity value. Please enter a positive number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt_activity.Clear();
+                txt_activity.Focus();
+                return;
+            }
+
+            query = "insert into Questions(activity, qno, question, option1, option2, option3, option4, ans) values('" + activity + "','" + qno + "','" + question + "','" + option1 + "','" + option2 + "','" + option3 + "','" + option4 + "','" + ans + "')";
                 fn.setData(query, "Question added");
 
                 clearAll();
@@ -101,18 +127,32 @@ namespace Online_Evaluation_System.Lecturer_usercontrols
 
         private void btn_finish_Click(object sender, EventArgs e)
         {
-            if(MessageBox.Show("Set Will be Changed", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
-            {
-                long activity;
+            
+            long activity;
                 if (long.TryParse(txt_activity.Text, out activity))
                 {
-                    txt_activity.Text = (activity + 1).ToString();
+                    if (activity == 0)
+                    {
+                        MessageBox.Show("Cannot enter 0. Please enter a positive number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txt_activity.Clear();
+                        txt_activity.Focus();
+                        return;
+                    }
+                    
                 }
                 else
                 {
                     MessageBox.Show("Invalid activity value", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     txt_activity.Clear();
+                    txt_activity.Focus();
+                    return;
                 }
+
+            
+
+            if (MessageBox.Show("Set Will be Changed", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                txt_activity.Text = (activity + 1).ToString();
                 lbl_questionNo.Text = "1";
             }
         }
